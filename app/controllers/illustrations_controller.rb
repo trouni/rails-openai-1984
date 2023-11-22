682ccdcd7c99f0b29833c8c242a1d1a0c5d5a4b4
Trouni Tiet <trouni@gmail.com>
2023-11-23 00:09:53 +0900

    Remove solution

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -1,12 +1,4 @@
 class IllustrationsController < ApplicationController
-  def create
-    @character = Character.find(params[:character_id])
-    detailed_situation = GenerateSituation.new(@character.description, params[:situation]).call
-    illustration_url = GenerateIllustration.new(detailed_situation).call
-    @character.attach_illustration_from_url(illustration_url)
-    redirect_to character_path(@character)
-  end
-  
   def destroy
     @illustration = ActiveStorage::Attachment.find(params[:id])
     @character = @illustration.record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  resources :characters, only: %i[show create] do
-    resources :illustrations, only: :create
-  end
+  resources :characters, only: %i[show create]
   resources :illustrations, only: :destroy
 end
