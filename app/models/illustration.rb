d90dac156ec12a8e6fc96a0eaccc9319b1a3c306
Trouni Tiet <trouni@gmail.com>
2023-11-22 17:13:45 +0900

    Use Cloudinary

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -2,4 +2,10 @@ require 'open-uri'
 
 class Illustration < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :character_photo
+  has_many_attached :images
+
+  validates :situation, presence: true
+  validates :character_photo, presence: true
 end
