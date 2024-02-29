77884f182ee9ef3d426100078172ca60b7551356
Trouni Tiet <trouni@gmail.com>
2024-03-01 02:14:06 +0900

    Remove solution

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -1,21 +1,8 @@
 class IllustrationsController < ApplicationController
-  def create
-    @character = Character.find(params[:character_id])
-    GenerateIllustrationJob.perform_later(@character.id, params[:situation])
-
-    redirect_to character_path(@character), notice: 'Your illustration is being generated...'
-  end
-  
   def destroy
     @illustration = ActiveStorage::Attachment.find(params[:id])
     @character = @illustration.record
     @illustration.purge
     redirect_to character_path(@character), notice: 'The illustration has been deleted!'
-  end
-
-  private
-
-  def character_params
-    params.require(:character).permit(:photo)
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,18 +1,3 @@
-require 'open-uri'
-
 class Character < ApplicationRecord
-  after_commit :generate_description, on: :create
-
-  has_one_attached :photo
   has_many_attached :illustrations
-
-  validates :photo, presence: true
-
-  def generate_description
-    UpdateCharacterDescriptionJob.perform_later(id)
-  end
-
-  def attach_illustration_from_url(url)
-    illustrations.attach(io: URI.open(url), filename: "heroifyme_#{SecureRandom.hex(8)}.png")
-  end
 end
